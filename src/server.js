ab8cd1282cdceb054d0073f0a6c3686d34bbb070
YesYourShin <ehrl827@naver.com>
2022-03-03 11:51:17 +0900

    Frontend Setup 절반

--- src/server.js
+++ src/server.js
@@ -4,4 +4,14 @@ const app = express();
 
 console.log("Hello");
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // static assets
+
+app.use("/public", express.static(__dirname + "/public"));
+
+function handleReq(req, res) {
+    res.render("home");
+}
+app.get("/", handleReq);
+
 app.listen(3000);
