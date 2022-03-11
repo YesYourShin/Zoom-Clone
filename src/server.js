b1cfade196ae02a8866c550017cecd3a5779f8f7
YesYourShin <ehrl827@naver.com>
2022-03-11 11:50:37 +0900

    nickname

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
-import http from "http";
-import WebSocket from "ws";
-import express from "express";
-import res from "express/lib/response";
+import http from 'http';
+import WebSocket from 'ws';
+import express from 'express';
+import res from 'express/lib/response';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views"); // static assets
-app.use("/public", express.static(__dirname + "/public"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views'); // static assets
+app.use('/public', express.static(__dirname + '/public'));
 // function handleReq(req, res) {
 //     res.render("home");
 // }
 // app.get("/", handleReq);
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
@@ -21,17 +21,22 @@ const wss = new WebSocket.Server({ server });
 
 const sockets = [];
 
-wss.on("connection", socket => {
+wss.on('connection', socket => {
     sockets.push(socket);
-    console.log("Connected to Browser");
-    socket.on("close", () => {
-        console.log("Disconnected from the Browser");
+    socket['nickname'] = 'Anon';
+    console.log('Connected to Browser');
+    socket.on('close', () => {
+        console.log('Disconnected from the Browser');
     });
-    socket.on("message", message => {
-        sockets.forEach(aSocket => aSocket.send(message.toString("utf8")));
+    socket.on('message', msg => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case 'new_message':
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case 'nickname':
+                socket['nickname'] = message.payload;
+        }
     });
 });
 
 server.listen(3000, handleListen);
-
-// app.listen(3000);
